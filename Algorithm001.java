0bc7b61305a5fe7eec26e85ee9c9195f256f1517
Yuto Sahashi <sahashi2002@gmail.com>
2021-11-30 13:26:15 +0900

    Fix Think

--- Algorithm001.java
+++ Algorithm001.java
@@ -6,7 +6,30 @@ class Algorithm001 extends Interface {
     }
 
     public void Think() {
+        if (Board.IsLastAttack(!alphaSide)){
+            switch (Board.GetLastAttackResult(!alphaSide)){
+                case 3:
+                    allySumHp--;
+                    allyCount--;
+                    break;
+                case 2:
+                    allySumHp--;
+                    break;
+            }
+        }
+        if (Board.IsLastAttack(alphaSide)){
+            switch (Board.GetLastAttackResult(alphaSide)){
+                case 3:
+                    enemySumHp--;
+                    enemyCount--;
+                    break;
+                case 2:
+                    enemySumHp--;
+                    break;
+            }
+        }
         Board.AttackPointsSearch(alphaSide);
+
         if (Board.IsLastMove(!alphaSide)) {
             if (alphaSide) {
                 Board.ClearValue(alphaSide);
@@ -22,25 +45,20 @@ class Algorithm001 extends Interface {
             }
             switch (Board.GetLastAttackResult(!alphaSide)) {
                 case 3:
-                case 2:
-                    allySumHp--;
-                    if (Board.GetLastAttackResult(!alphaSide) == 3) {
-                        allyCount--;
-                        // 敵に撃沈された
-                    } else {
-                        // 敵に命中された
-                        ArrayList<Point> points = new ArrayList<Point>();
-                        for (Point point : Board.PointCross(Board.GetLastAttackPoint(!alphaSide), 2)) {
-                            if (Board.IsMoveEnablePoint(alphaSide, Board.GetLastAttackPoint(!alphaSide),
-                                    point)) {
-                                points.add(point);
-                            }
-                        }
-                        // 移動できる範囲からランダムに移動
-                        DoMove(Board.GetLastAttackPoint(!alphaSide), Board.RandomGet(points));
-                        return;
-                    }
+                    // 敵に撃沈された
                     break;
+                case 2:
+                    // 敵に命中された
+                    ArrayList<Point> points = new ArrayList<Point>();
+                    for (Point point : Board.PointCross(Board.GetLastAttackPoint(!alphaSide), 2)) {
+                        if (Board.IsMoveEnablePoint(alphaSide, Board.GetLastAttackPoint(!alphaSide),
+                                point)) {
+                            points.add(point);
+                        }
+                    }
+                    // 移動できる範囲からランダムに移動
+                    DoMove(Board.GetLastAttackPoint(!alphaSide), Board.RandomGet(points));
+                    return;
                 case 1:
                     // 波高しされた
 
@@ -51,37 +69,33 @@ class Algorithm001 extends Interface {
             // 敵を攻撃した
             switch (Board.GetLastAttackResult(alphaSide)) {
                 case 3:
+                    // 敵を撃沈した
+                    Board.GetCell(Board.GetLastAttackPoint(alphaSide)).SetValue(alphaSide, 0);
+                    break;
                 case 2:
-                    enemySumHp--;
-                    if (Board.GetLastAttackResult(alphaSide) == 3) {
-                        enemyCount--;
-                        // 敵を撃沈した
-                        Board.GetCell(Board.GetLastAttackPoint(alphaSide)).SetValue(alphaSide, 0);
-                    } else {
-                        // 敵を命中した
-                        Board.GetCell(Board.GetLastAttackPoint(alphaSide)).SetValue(alphaSide, 10);
-                        if (Board.IsLastMove(!alphaSide)) {
-                            // 敵が移動した
-                            if (enemyCount == 1) {
-                                // 敵が1機のみ
-                                Board.GetCell(Board.GetLastAttackPoint(alphaSide)).SetValue(alphaSide, 0);
-                                Board.GetCell(Board.GetLastAttackPoint(alphaSide)
-                                        .Plus(Board.GetLastMoveVector(!alphaSide))).SetValue(alphaSide, 10);
-                                if (Board.IsAttackPoint(alphaSide, Board.GetLastAttackPoint(alphaSide)
-                                        .Plus(Board.GetLastMoveVector(!alphaSide)))) {
-                                    // 攻撃可能範囲内なら攻撃する
-                                    DoAttack(Board.GetLastAttackPoint(alphaSide)
-                                            .Plus(Board.GetLastMoveVector(!alphaSide)));
-                                    return;
-                                }
-                            } else {
-                                // 敵が2機以上
+                    // 敵を命中した
+                    Board.GetCell(Board.GetLastAttackPoint(alphaSide)).SetValue(alphaSide, 10);
+                    if (Board.IsLastMove(!alphaSide)) {
+                        // 敵が移動した
+                        if (enemyCount == 1) {
+                            // 敵が1機のみ
+                            Board.GetCell(Board.GetLastAttackPoint(alphaSide)).SetValue(alphaSide, 0);
+                            Board.GetCell(Board.GetLastAttackPoint(alphaSide)
+                                    .Plus(Board.GetLastMoveVector(!alphaSide))).SetValue(alphaSide, 10);
+                            if (Board.IsAttackPoint(alphaSide, Board.GetLastAttackPoint(alphaSide)
+                                    .Plus(Board.GetLastMoveVector(!alphaSide)))) {
+                                // 攻撃が可能なら攻撃する
+                                DoAttack(Board.GetLastAttackPoint(alphaSide)
+                                        .Plus(Board.GetLastMoveVector(!alphaSide)));
+                                return;
                             }
                         } else {
-                            // 敵が移動しなかった
-                            DoAttack(Board.GetLastAttackPoint(alphaSide));
-                            return;
+                            // 敵が2機以上
                         }
+                    } else {
+                        // 敵が移動しなかった
+                        DoAttack(Board.GetLastAttackPoint(alphaSide));
+                        return;
                     }
                     break;
                 case 1:
